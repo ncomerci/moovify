55f6d3aba437af7cbb5392d16dba73f8c2b33028
Ignacio <isagues@itba.edu.ar>
2020-10-03 12:04:20 -0300

    Added pageSize selector to search/post view.

--- webapp/src/main/webapp/resources/js/search/posts.js
+++ webapp/src/main/webapp/resources/js/search/posts.js
@@ -4,9 +4,11 @@ window.addEventListener('load', function(){
     let postCategory = document.getElementById('postCategory');
     let postAge = document.getElementById('postAge');
     let sortCriteria = document.getElementById('sortCriteria');
+    let pageSize = document.getElementById('pageSize');
 
     postCategory.addEventListener('change',() => form.submit(),false);
     postAge.addEventListener('change',() => form.submit(),false);
     sortCriteria.addEventListener('change',() => form.submit(),false);
+    pageSize.addEventListener('change',() => form.submit(),false);
 
 }, false);
