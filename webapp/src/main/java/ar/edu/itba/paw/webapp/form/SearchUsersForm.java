4592957f2001d1f2f265066859e0ee520f1624b3
Tobias <tbrandy@itba.edu.ar>
2020-10-06 01:23:26 -0300

    Fixed pagination not conserving order by when attribute wasn't part of the original table. (For example, total_likes in users). Search engine fully implemented for Users

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/SearchUsersForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/SearchUsersForm.java
@@ -7,11 +7,31 @@ public class SearchUsersForm {
     @NotNull
     private String query;
 
+    private String role;
+
+    private String sortCriteria;
+
     public String getQuery() {
         return query;
     }
 
     public void setQuery(String query) {
         this.query = query;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSortCriteria() {
+        return sortCriteria;
+    }
+
+    public void setSortCriteria(String sortCriteria) {
+        this.sortCriteria = sortCriteria;
     }
 }
