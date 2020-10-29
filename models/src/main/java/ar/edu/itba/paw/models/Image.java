077e0085433fe60d200bbc4500e2e72eb6b16899
Tobias <tbrandy@itba.edu.ar>
2020-10-29 02:49:05 -0300

    UserDaoImpl now working without pagination. Still lacks exception on register.

--- models/src/main/java/ar/edu/itba/paw/models/Image.java
+++ models/src/main/java/ar/edu/itba/paw/models/Image.java
@@ -1,7 +1,5 @@
 package ar.edu.itba.paw.models;
 
-import org.hibernate.annotations.Type;
-
 import javax.persistence.*;
 
 @Entity
@@ -14,10 +12,7 @@ public class Image {
     @Column(name = "image_id")
     private Long id;
 
-    // TODO: Decide
-    @Lob
-    @Type(type = "org.hibernate.type.BinaryType")
-    @Column(name = "image", nullable = false, columnDefinition="BLOB") //TODO puede haber error usando BLOB aca
+    @Column(name = "image", nullable = false, columnDefinition="BLOB")
     @Basic(fetch = FetchType.LAZY, optional = false)
     private byte[] data;
 
--- persistence/src/main/java/ar/edu/itba/paw/persistence/UserDaoImpl.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/UserDaoImpl.java
@@ -3,17 +3,13 @@ package ar.edu.itba.paw.persistence;
 import ar.edu.itba.paw.interfaces.persistence.UserDao;
 import ar.edu.itba.paw.interfaces.persistence.exceptions.DuplicateEmailException;
 import ar.edu.itba.paw.interfaces.persistence.exceptions.DuplicateUsernameException;
-import ar.edu.itba.paw.models.Image;
-import ar.edu.itba.paw.models.PaginatedCollection;
-import ar.edu.itba.paw.models.Role;
-import ar.edu.itba.paw.models.User;
+import ar.edu.itba.paw.models.*;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import javax.persistence.Tuple;
+import javax.persistence.criteria.*;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Collections;
@@ -27,8 +23,8 @@ public class UserDaoImpl implements UserDao {
     @PersistenceContext
     private EntityManager em;
 
-    private static final EnumMap<SortCriteria,String> sortCriteriaQueryMap = initializeSortCriteriaQueryMap();
-    private static final EnumMap<SortCriteria,String> sortCriteriaHQLMap = initializeSortCriteriaHQLMap();
+    private static final EnumMap<SortCriteria, String> sortCriteriaQueryMap = initializeSortCriteriaQueryMap();
+    private static final EnumMap<SortCriteria, QueryBlockExecutor> sortCriteriaQueryExecutorMap = initializeSortCriteriaQueryExecutorMap();
 
     private static EnumMap<SortCriteria, String> initializeSortCriteriaQueryMap() {
 
@@ -42,17 +38,20 @@ public class UserDaoImpl implements UserDao {
         return sortCriteriaQuery;
     }
 
-    private static EnumMap<SortCriteria, String> initializeSortCriteriaHQLMap() {
+    private static EnumMap<SortCriteria, QueryBlockExecutor> initializeSortCriteriaQueryExecutorMap() {
 
-        EnumMap<SortCriteria, String> sortCriteriaQuery = new EnumMap<>(SortCriteria.class);
+        EnumMap<SortCriteria, QueryBlockExecutor> sortCriteriaQuery = new EnumMap<>(SortCriteria.class);
 
-        sortCriteriaQuery.put(SortCriteria.NEWEST, "u.creation_date desc");
-        sortCriteriaQuery.put(SortCriteria.OLDEST, "u.creation_date");
-        sortCriteriaQuery.put(SortCriteria.LIKES, "TOTAL_LIKES.total_likes desc");
-        sortCriteriaQuery.put(SortCriteria.USERNAME, "u.username");
+        sortCriteriaQuery.put(SortCriteria.NEWEST, (cb, q, u, tl) -> q.orderBy(cb.desc(u.get("creationDate"))) );
+        sortCriteriaQuery.put(SortCriteria.OLDEST, (cb, q, u, tl) -> q.orderBy(cb.asc(u.get("creationDate"))) );
+        sortCriteriaQuery.put(SortCriteria.LIKES, (cb, q, u, tl) -> q.orderBy(cb.desc(tl)) );
+        sortCriteriaQuery.put(SortCriteria.USERNAME, (cb, q, u, tl) -> q.orderBy(cb.asc(u.get("username"))) );
 
         return sortCriteriaQuery;
     }
+
+    private static final PredicateSupplier<String> queryEqualsUsername =
+            (cb, u, tl, query) -> cb.like(cb.lower(u.get("name")), cb.concat("%", cb.concat(cb.lower(cb.literal(query)), "%")));
 
 //    TODO: ver como hacer para chequear el unique email y el unique username
     @Override
@@ -87,6 +86,72 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public PaginatedCollection<User> getAllUsers(SortCriteria sortCriteria, int pageNumber, int pageSize) {
+
+        final Collection<User> users = queryUsers(null, sortCriteria);
+
+        return new PaginatedCollection<>(users, pageNumber, pageSize, users.size());
+    }
+
+    @Override
+    public PaginatedCollection<User> searchUsers(String query, SortCriteria sortCriteria, int pageNumber, int pageSize) {
+
+        final Collection<User> users =
+                queryUsers((cb, q, u, tl) -> q.where(
+                            queryEqualsUsername.get(cb, u, tl, query)
+
+                        ), sortCriteria);
+
+        return new PaginatedCollection<>(users, pageNumber, pageSize, users.size());
+    }
+
+    @Override
+    public PaginatedCollection<User> searchUsersByRole(String query, Role role, SortCriteria sortCriteria, int pageNumber, int pageSize) {
+
+        final Collection<User> users =
+                queryUsers(
+                        (cb, q, u, tl) -> {
+
+                            Subquery<Long> sq = q.subquery(Long.class);
+                            Root<User> su = sq.from(User.class);
+                            Join<User, Role> roles = su.join("roles", JoinType.INNER);
+                            sq.select(su.get("id")).where(cb.literal(role.name()).in(roles));
+
+                            q.where(
+                                    queryEqualsUsername.get(cb, u, tl, query),
+                                    cb.in(u.get("id")).value(sq)
+                            );
+                            }, sortCriteria);
+
+        return new PaginatedCollection<>(users, pageNumber, pageSize, users.size());
+    }
+
+    @Override
+    public PaginatedCollection<User> searchDeletedUsers(String query, SortCriteria sortCriteria, int pageNumber, int pageSize) {
+
+        final Collection<User> users =
+                queryUsers((cb, q, u, tl) -> q.where(
+                        queryEqualsUsername.get(cb, u, tl, query),
+                        cb.equal(u.get("enabled"), false)
+
+                ), sortCriteria);
+
+        return new PaginatedCollection<>(users, pageNumber, pageSize, users.size());
+    }
+
+    private Collection<User> queryUsers(QueryBlockExecutor whereQueryBlock, SortCriteria sortCriteria) {
+        // Original Query
+        /*
+        final Collection<Object[]> queryResult = em.createQuery(
+                "SELECT u, sum(coalesce(commentLikes.value, 0) + coalesce(postLikes.value, 0)) AS totalLikes" +
+                        " FROM User u LEFT OUTER JOIN u.commentLikes commentLikes LEFT OUTER JOIN u.postLikes postLikes" +
+                          --- CUSTOM WHERE ---
+                        " GROUP BY u
+                          --- CUSTOM ORDER BY ---
+                        , Object[].class)
+                .getResultList();
+        */
+
+        //TODO: Falta paginacion
         /*Query pagingQuery = em.createNativeQuery("SELECT * FROM users ORDER BY user_id LIMIT "
                 + String.valueOf(pageSize) + " OFFSET " + String.valueOf(pageNumber));
 
@@ -96,35 +161,47 @@ public class UserDaoImpl implements UserDao {
         TypedQuery<User> query = em.createQuery("select u from User u where u.id IN :idList", User.class)
                 .setParameter("idList", resultList);*/
 
+        final CriteriaBuilder cb = em.getCriteriaBuilder();
 
-        final Collection<User> users = queryUsers();
+        final CriteriaQuery<Tuple> q = cb.createTupleQuery();
 
-        return new PaginatedCollection<>(users, pageNumber, pageSize, users.size());
-    }
+        // From And Joins
+        final Root<User> u = q.from(User.class);
 
-    @Override
-    public PaginatedCollection<User> searchUsers(String query, SortCriteria sortCriteria, int pageNumber, int pageSize) {
-        return null;
-    }
+        final Join<User, CommentLike> commentLikes = u.join("commentLikes", JoinType.LEFT);
 
-    @Override
-    public PaginatedCollection<User> searchUsersByRole(String query, Role role, SortCriteria sortCriteria, int pageNumber, int pageSize) {
-        return null;
-    }
+        final Join<User, PostLike> postLikes = u.join("postLikes", JoinType.LEFT);
 
-    @Override
-    public PaginatedCollection<User> searchDeletedUsers(String query, SortCriteria sortCriteria, int pageNumber, int pageSize) {
-        return null;
-    }
+        // Preparing Select Aggregated Parameter
+        final Expression<Long> totalLikes = cb.sumAsLong(
+                        cb.sum(
+                                cb.coalesce(commentLikes.get("value"), 0),
+                                cb.coalesce(postLikes.get("value"), 0)
+                        )
+                );
 
-    private Collection<User> queryUsers() {
-        final Collection<Object[]> queryResult = em.createQuery(
-                "SELECT u, sum(commentLikes.value + postLikes.value) AS totalLikes FROM User u LEFT OUTER JOIN u.commentLikes commentLikes LEFT OUTER JOIN u.postLikes postLikes GROUP BY u ORDER BY totalLikes DESC", Object[].class)
-                .getResultList();
+        // Select
+        q.multiselect(u, totalLikes);
 
-        return queryResult.stream().map(tuple -> {
-            ((User) tuple[0]).setTotalLikes((long) tuple[1]);
-            return ((User) tuple[0]);
+        // Where
+        if(whereQueryBlock != null)
+            whereQueryBlock.execute(cb, q, u, totalLikes);
+
+        // Group By
+        q.groupBy(u);
+
+        // Order By
+        sortCriteriaQueryExecutorMap.get(sortCriteria).execute(cb, q, u, totalLikes);
+
+        // Execute Query
+        Collection<Tuple> tuples = em.createQuery(q).getResultList();
+
+        // Map Tuples To Users
+        return tuples.stream().map(tuple -> {
+
+            tuple.get(u).setTotalLikes(tuple.get(totalLikes));
+            return tuple.get(u);
+
         }).collect(Collectors.toList());
     }
 
@@ -140,5 +217,17 @@ public class UserDaoImpl implements UserDao {
         q.where(cb.equal(u.get(field), value), cb.equal(u.get("enabled"), enabled));
 
         return em.createQuery(q).getResultList().stream().findFirst();
+    }
+
+    @FunctionalInterface
+    private interface PredicateSupplier<T> {
+
+        Predicate get(CriteriaBuilder cb, Root<User> u, Expression<Long> totalLikes, T param);
+    }
+
+    @FunctionalInterface
+    private interface QueryBlockExecutor {
+
+        void execute(CriteriaBuilder cb, CriteriaQuery<Tuple> q, Root<User> u, Expression<Long> totalLikes);
     }
 }
