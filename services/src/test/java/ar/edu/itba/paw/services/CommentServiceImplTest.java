9e603a9860a6f19fb06be152d5ff55784ebeb07f
Tobias <tbrandy@itba.edu.ar>
2020-11-20 01:38:40 -0300

    Fixed Service Tests Regarding New Exception Handling

--- services/src/test/java/ar/edu/itba/paw/services/CommentServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/CommentServiceImplTest.java
@@ -2,7 +2,9 @@ package ar.edu.itba.paw.services;
 
 import ar.edu.itba.paw.interfaces.persistence.CommentDao;
 import ar.edu.itba.paw.interfaces.services.MailService;
+import ar.edu.itba.paw.interfaces.services.exceptions.IllegalCommentEditionException;
 import ar.edu.itba.paw.interfaces.services.exceptions.IllegalCommentLikeException;
+import ar.edu.itba.paw.interfaces.services.exceptions.MissingCommentEditPermissionException;
 import ar.edu.itba.paw.models.Comment;
 import ar.edu.itba.paw.models.User;
 import org.junit.Test;
@@ -33,12 +35,39 @@ public class CommentServiceImplTest {
      * y no tenemos forma de controlar el output del constructor.
      */
 
+    @Test(expected = IllegalCommentEditionException.class)
+    public void testLikeEditDisabledComment() throws MissingCommentEditPermissionException, IllegalCommentEditionException {
+
+        Comment comment = Mockito.mock(Comment.class);
+        User user = Mockito.mock(User.class);
+
+        Mockito.when(comment.isEnabled()).thenReturn(false);
+
+        commentService.editComment(user, comment, "");
+    }
+
+    @Test(expected = MissingCommentEditPermissionException.class)
+    public void testLikeEditEditorWithoutPermission() throws MissingCommentEditPermissionException, IllegalCommentEditionException {
+
+        Comment comment = Mockito.mock(Comment.class);
+        User user = Mockito.mock(User.class);
+
+        Mockito.when(comment.isEnabled()).thenReturn(true);
+        Mockito.when(comment.getUser()).thenReturn(Mockito.mock(User.class));
+
+        commentService.editComment(user, comment, "");
+    }
+
     @Test
     public void testLikeCommentRemove() throws IllegalCommentLikeException {
 
         Comment comment = Mockito.mock(Comment.class);
+        User user = Mockito.mock(User.class);
 
-        commentService.likeComment(comment, Mockito.mock(User.class),0);
+        Mockito.when(comment.isEnabled()).thenReturn(true);
+        Mockito.when(comment.getLikeValue(user)).thenReturn(UP_VOTE_VALUE);
+
+        commentService.likeComment(comment, user,0);
 
         Mockito.verify(comment).removeLike(Mockito.any());
     }
@@ -48,6 +77,9 @@ public class CommentServiceImplTest {
 
         Comment comment = Mockito.mock(Comment.class);
         User user = Mockito.mock(User.class);
+
+        Mockito.when(comment.isEnabled()).thenReturn(true);
+        Mockito.when(comment.getLikeValue(user)).thenReturn(0);
 
         commentService.likeComment(comment, user, UP_VOTE_VALUE);
 
@@ -60,8 +92,22 @@ public class CommentServiceImplTest {
         Comment comment = Mockito.mock(Comment.class);
         User user = Mockito.mock(User.class);
 
+        Mockito.when(comment.isEnabled()).thenReturn(true);
+        Mockito.when(comment.getLikeValue(user)).thenReturn(0);
+
         commentService.likeComment(comment, user, DOWN_VOTE_VALUE);
 
         Mockito.verify(comment).like(Mockito.eq(user), Mockito.eq(DOWN_VOTE_VALUE));
+    }
+
+    @Test(expected = IllegalCommentLikeException.class)
+    public void testLikeCommentDisabledComment() throws IllegalCommentLikeException {
+
+        Comment comment = Mockito.mock(Comment.class);
+        User user = Mockito.mock(User.class);
+
+        Mockito.when(comment.isEnabled()).thenReturn(false);
+
+        commentService.likeComment(comment, user, DOWN_VOTE_VALUE);
     }
 }
