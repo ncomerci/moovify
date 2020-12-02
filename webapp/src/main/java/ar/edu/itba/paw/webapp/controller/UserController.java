2833c2944b28ba6c08abc359d1bb622f78df238e
Tobias <tbrandy@itba.edu.ar>
2020-12-02 15:17:57 -0300

    Server Side Scaffolding Done.

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
@@ -1,35 +1,77 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.interfaces.persistence.exceptions.DuplicateUniqueUserAttributeException;
 import ar.edu.itba.paw.interfaces.services.UserService;
 import ar.edu.itba.paw.models.PaginatedCollection;
 import ar.edu.itba.paw.models.User;
+import ar.edu.itba.paw.webapp.dto.UserDto;
+import ar.edu.itba.paw.webapp.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
 import java.util.Collection;
 
 @Path("users")
 @Component
 public class UserController {
 
+    @Context
+    private UriInfo uriInfo;
+
     @Autowired
     private UserService userService;
 
     @Produces(MediaType.APPLICATION_JSON)
     @GET
-    public Response listUsers() {
-        final PaginatedCollection<User> users = userService.getAllUsers(0, 5);
+    public Response listUsers(@QueryParam("pageNumber") @DefaultValue("0") int pageNumber,
+                              @QueryParam("pageSize") @DefaultValue("10") int pageSize) {
+
+        final UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
+
+        final PaginatedCollection<User> users = userService.getAllUsers(pageNumber, pageSize);
 
         if(users.getResults().isEmpty())
             return Response.noContent().build();
 
-        return Response.ok(new GenericEntity<Collection<User>>(users.getResults()) {}).build();
+        return Response.ok(new GenericEntity<Collection<UserDto>>(UserDto.mapUsersToDto(users.getResults(), uriInfo)) {})
+                .link(
+                        uriBuilder
+                                .queryParam("pageNumber", pageNumber + 1)
+                                .queryParam("pageSize", pageSize)
+                        .build(),
+                        "next")
+                .build();
+    }
+
+    @Produces(MediaType.APPLICATION_JSON)
+    @GET
+    @Path("/{id}")
+    public Response getUser(@PathParam("id") long id) {
+
+        final User user = userService.findUserById(id).orElseThrow(UserNotFoundException::new);
+
+        return Response.ok(new UserDto(user, uriInfo)).build();
+    }
+
+    @Produces(MediaType.APPLICATION_JSON)
+    @POST
+    public Response createUser(final UserDto userDto, @Context HttpServletRequest request) {
+
+        final User user;
+
+        try {
+            user = userService.register(userDto.getUsername(), userDto.getPassword(), userDto.getName(), userDto.getEmail(),
+                    userDto.getDescription(), new byte[0], "confirmEmail", request.getLocale());
+        }
+        catch(DuplicateUniqueUserAttributeException e) {
+            // TODO
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        return Response.ok(new UserDto(user, uriInfo)).build();
     }
 
 }
---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dto/UserDto.java
@@ -0,0 +1,136 @@
+package ar.edu.itba.paw.webapp.dto;
+
+import ar.edu.itba.paw.models.User;
+
+import javax.ws.rs.core.UriInfo;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class UserDto {
+
+    public static Collection<UserDto> mapUsersToDto(Collection<User> users, UriInfo uriInfo) {
+        return users.stream().map(u -> new UserDto(u, uriInfo)).collect(Collectors.toList());
+    }
+
+    private long id;
+    private LocalDateTime creationDate;
+    private String username;
+    private String name;
+    private String email;
+    private String description;
+    private String language;
+    private long totalLikes;
+    private boolean enabled;
+
+    // Not sent
+    private String password;
+
+    private String url;
+
+    public UserDto() {
+        // Do not use
+    }
+
+    public UserDto(User user, UriInfo uriInfo) {
+        id = user.getId();
+        creationDate = user.getCreationDate();
+        username = user.getUsername();
+        name = user.getName();
+        email = user.getEmail();
+        description = user.getDescription();
+        language = user.getLanguage();
+        totalLikes = user.getTotalLikes();
+        enabled = user.isEnabled();
+
+        url = uriInfo.getBaseUriBuilder().path("users").path(String.valueOf(id)).build().toString();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public long getTotalLikes() {
+        return totalLikes;
+    }
+
+    public void setTotalLikes(long totalLikes) {
+        this.totalLikes = totalLikes;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
