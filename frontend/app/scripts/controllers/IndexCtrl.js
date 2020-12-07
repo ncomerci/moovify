0e2bf3b4291ed2756f38739922ddada870fff8c5
Nicolás <ncomerci@itba.edu.ar>
2020-12-07 19:38:20 -0300

    fix

--- frontend/app/scripts/controllers/IndexCtrl.js
+++ frontend/app/scripts/controllers/IndexCtrl.js
@@ -4,10 +4,5 @@ define(['frontend'], function(frontend) {
 	frontend.controller('IndexCtrl', function($scope, Restangular) {
 		Restangular.one('posts', 1).get().then(n => console.log(n.title));
 		$scope.welcomeText = 'Welcome to your frontend page';
-
-		$scope.loading = true;
-    $scope.$on('$viewContentLoaded', function(){
-      $scope.loading = false;
-    });
 	});
 });
