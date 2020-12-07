f47cbc2d1dc243e34e80672210743be4aa5993b1
Nicolás <ncomerci@itba.edu.ar>
2020-12-07 19:37:13 -0300

    preventing load angular components before angular is fully loaded

--- frontend/app/scripts/build.js
+++ frontend/app/scripts/build.js
@@ -14,7 +14,21 @@ require.config({
         requirejs: '../../bower_components/requirejs/require',
         restangular: '../../bower_components/restangular/dist/restangular',
         uikit: '../../bower_components/uikit/dist/js/uikit',
-        uikiticons: '../../bower_components/uikit/dist/js/uikit-icons'
+        uikiticons: '../../bower_components/uikit/dist/js/uikit-icons',
+        bootstrap: '../../bower_components/bootstrap/dist/js/bootstrap',
+        affix: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/affix',
+        alert: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/alert',
+        button: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/button',
+        carousel: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/carousel',
+        collapse: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/collapse',
+        dropdown: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/dropdown',
+        tab: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tab',
+        transition: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/transition',
+        scrollspy: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/scrollspy',
+        modal: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/modal',
+        tooltip: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tooltip',
+        popover: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/popover',
+        'uikit-icons': '../../bower_components/uikit/dist/js/uikit-icons'
     },
     shim: {
         angular: {
