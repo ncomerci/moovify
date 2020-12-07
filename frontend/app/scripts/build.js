bb5fecec0b33853ce21452adf0653ef590798c29
Nicolás <ncomerci@itba.edu.ar>
2020-12-07 01:20:22 -0300

    navbar

--- frontend/app/scripts/build.js
+++ frontend/app/scripts/build.js
@@ -13,7 +13,19 @@ require.config({
         requirejs: '../../bower_components/requirejs/require',
         uikit: '../../bower_components/uikit/dist/js/uikit',
         uikiticons: '../../bower_components/uikit/dist/js/uikit-icons',
-
+        bootstrap: '../../bower_components/bootstrap/dist/js/bootstrap',
+        affix: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/affix',
+        alert: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/alert',
+        button: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/button',
+        carousel: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/carousel',
+        collapse: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/collapse',
+        dropdown: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/dropdown',
+        tab: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tab',
+        transition: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/transition',
+        scrollspy: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/scrollspy',
+        modal: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/modal',
+        tooltip: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tooltip',
+        popover: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/popover'
     },
     shim: {
         angular: {
@@ -26,7 +38,7 @@ require.config({
                 'angular'
             ]
         },
-        'uikiticons': {
+        uikiticons: {
             deps: [
                 'uikit'
             ]
