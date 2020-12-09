9f5908f7a3c6d40beef533d619a6a4faeb3af4ad
Nicolás <ncomerci@itba.edu.ar>
2020-12-09 12:55:27 -0300

    Revert "Feature remember me"
    This reverts commit 06321bef

--- frontend/app/scripts/controllers/IndexCtrl.js
+++ frontend/app/scripts/controllers/IndexCtrl.js
@@ -1,14 +1,17 @@
 'use strict';
 define(['frontend', 'services/LoginService', 'services/PageTitleService'], function(frontend) {
 
-	frontend.controller('IndexCtrl', function($scope, LoggedUserFactory, $route, PageTitle) {
+	frontend.controller('IndexCtrl', function($scope, LoggedUserFactory, $route, PageTitle, $window) {
 		$scope.welcomeText = 'Welcome to your frontend page';
 		$scope.loggedUser = LoggedUserFactory.getLoggedUser();
 		PageTitle.setTitle('asdasd'); // TODO: cambiar key
-    $scope.$on('$routeChangeSuccess', function () {
-      $scope.title = PageTitle.getTitle();
+    $scope.title = PageTitle.getTitle();
 
-    });
+    const token = $window.localStorage.getItem("authorization");
+    if(token) {
+      LoggedUserFactory.saveToken(token);
+    }
+
 	});
 
 });
