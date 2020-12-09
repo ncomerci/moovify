9918a4ff973dff5a2b78dff2402e65915b62914e
Nicolás <ncomerci@itba.edu.ar>
2020-12-08 23:44:03 -0300

    visual fixes

--- frontend/app/scripts/i18n/translations.es.js
+++ frontend/app/scripts/i18n/translations.es.js
@@ -4,12 +4,14 @@ define([], function() {
 	return {
     USER_LOGIN_TITLE: 'Moovify | Login',
     USER_LOGIN: 'Log in',
+    USER_LOGIN_ALT: 'Login',
     USER_LOGIN_USERNAME: 'Username',
     USER_LOGIN_PASSWORD: 'Password',
     USER_LOGIN_REMEMBERME: 'Remember me',
     USER_LOGIN_NOACCOUNT: 'Do not have an account yet?',
     USER_LOGIN_RESETPASSWORD: 'Forgot my password',
     USER_CREATE_SIGNUP: 'Sign up',
-
+    USER_CREATE_SIGNUP_ALT: 'Signup',
+    SEARCH_TITLE: 'Search'
 	};
 });
