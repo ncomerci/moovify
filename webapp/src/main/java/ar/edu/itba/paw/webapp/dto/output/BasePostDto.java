ac915d174152a76048223590dd959812b9bbb02d
Tobias <tbrandy@itba.edu.ar>
2020-12-10 15:34:21 -0300

    Added LightweightPostDto for relationships. Movies are not included

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dto/output/BasePostDto.java
@@ -0,0 +1,191 @@
+package ar.edu.itba.paw.webapp.dto.output;
+
+import ar.edu.itba.paw.models.Post;
+import ar.edu.itba.paw.models.Role;
+
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+public abstract class BasePostDto {
+
+    public static UriBuilder getPostUriBuilder(Post post, UriInfo uriInfo) {
+        return uriInfo.getBaseUriBuilder().path("posts").path(String.valueOf(post.getId()));
+    }
+
+    private long id;
+    private LocalDateTime creationDate;
+    private String title;
+    private String body;
+    private Integer wordCount;
+    private Boolean edited;
+    private LocalDateTime lastEditDate;
+    private UserDto user;
+    private PostCategoryDto postCategory;
+    private Collection<String> tags;
+    private boolean enabled;
+    private Long totalLikes;
+
+    // Relations
+    private String comments;
+    private String votes;
+
+
+    private String url;
+
+    public BasePostDto() {
+        // For Jersey - Do not use
+    }
+
+    public BasePostDto(Post post, UriInfo uriInfo, SecurityContext securityContext) {
+
+        final UriBuilder postUriBuilder = getPostUriBuilder(post, uriInfo);
+
+        id = post.getId();
+        enabled = post.isEnabled();
+        url = postUriBuilder.build().toString();
+
+        if(isProtected(securityContext))
+            return;
+
+        creationDate = post.getCreationDate();
+        title = post.getTitle();
+        body = post.getBody();
+        wordCount = post.getWordCount();
+        edited = post.isEdited();
+        lastEditDate = post.getLastEditDate();
+        user = new UserDto(post.getUser(), uriInfo, securityContext);
+        postCategory = new PostCategoryDto(post.getCategory());
+        tags = post.getTags();
+        totalLikes = post.getTotalVotes();
+
+        comments = postUriBuilder.clone().path("comments").build().toString();
+        votes = postUriBuilder.clone().path("votes").build().toString();
+    }
+
+    protected boolean isProtected(SecurityContext securityContext) {
+        return !enabled && !securityContext.isUserInRole(Role.ADMIN.name());
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Integer getWordCount() {
+        return wordCount;
+    }
+
+    public void setWordCount(Integer wordCount) {
+        this.wordCount = wordCount;
+    }
+
+    public Boolean isEdited() {
+        return edited;
+    }
+
+    public void setEdited(Boolean edited) {
+        this.edited = edited;
+    }
+
+    public LocalDateTime getLastEditDate() {
+        return lastEditDate;
+    }
+
+    public void setLastEditDate(LocalDateTime lastEditDate) {
+        this.lastEditDate = lastEditDate;
+    }
+
+    public UserDto getUser() {
+        return user;
+    }
+
+    public void setUser(UserDto user) {
+        this.user = user;
+    }
+
+    public PostCategoryDto getPostCategory() {
+        return postCategory;
+    }
+
+    public void setPostCategory(PostCategoryDto postCategory) {
+        this.postCategory = postCategory;
+    }
+
+    public Collection<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Collection<String> tags) {
+        this.tags = tags;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Long getTotalLikes() {
+        return totalLikes;
+    }
+
+    public void setTotalLikes(Long totalLikes) {
+        this.totalLikes = totalLikes;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getVotes() {
+        return votes;
+    }
+
+    public void setVotes(String votes) {
+        this.votes = votes;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
--- webapp/src/main/java/ar/edu/itba/paw/webapp/dto/output/PostDto.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dto/output/PostDto.java
@@ -1,201 +1,39 @@
 package ar.edu.itba.paw.webapp.dto.output;
 
 import ar.edu.itba.paw.models.Post;
-import ar.edu.itba.paw.models.Role;
 
 import javax.ws.rs.core.SecurityContext;
-import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-public class PostDto {
+public class PostDto extends BasePostDto {
 
     public static Collection<PostDto> mapPostsToDto(Collection<Post> posts, UriInfo uriInfo, SecurityContext securityContext) {
         return posts.stream().map(p -> new PostDto(p, uriInfo, securityContext)).collect(Collectors.toList());
     }
 
-    public static UriBuilder getPostUriBuilder(Post post, UriInfo uriInfo) {
-        return uriInfo.getBaseUriBuilder().path("posts").path(String.valueOf(post.getId()));
-    }
+    private Collection<MovieDto> movies;
 
-    private long id;
-    private LocalDateTime creationDate;
-    private String title;
-    private String body;
-    private Integer wordCount;
-    private Boolean edited;
-    private LocalDateTime lastEditDate;
-    private UserDto user;
-    private PostCategoryDto postCategory;
-    private Collection<String> tags;
-    private boolean enabled;
-    private Long totalLikes;
-
-    // Relations
-    private String comments;
-    private String votes;
-    private String movies;
-
-    private String url;
-
-    public PostDto(){
+    public PostDto() {
+        super();
         // For Jersey - Do not use
     }
 
     public PostDto(Post post, UriInfo uriInfo, SecurityContext securityContext) {
+        super(post, uriInfo, securityContext);
 
-        final UriBuilder postUriBuilder = getPostUriBuilder(post, uriInfo);
-
-        id = post.getId();
-        enabled = post.isEnabled();
-        url = postUriBuilder.build().toString();
-
-        if(!enabled && !securityContext.isUserInRole(Role.ADMIN.name()))
+        if(isProtected(securityContext))
             return;
 
-        creationDate = post.getCreationDate();
-        title = post.getTitle();
-        body = post.getBody();
-        wordCount = post.getWordCount();
-        edited = post.isEdited();
-        lastEditDate = post.getLastEditDate();
-        user = new UserDto(post.getUser(), uriInfo, securityContext);
-        postCategory = new PostCategoryDto(post.getCategory());
-        tags = post.getTags();
-        totalLikes = post.getTotalVotes();
-
-        comments = postUriBuilder.clone().path("comments").build().toString();
-        votes = postUriBuilder.clone().path("votes").build().toString();
-        movies = postUriBuilder.clone().path("movies").build().toString();
+        movies = MovieDto.mapMoviesToDto(post.getMovies(), uriInfo);
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public Integer getWordCount() {
-        return wordCount;
-    }
-
-    public void setWordCount(Integer wordCount) {
-        this.wordCount = wordCount;
-    }
-
-    public Boolean isEdited() {
-        return edited;
-    }
-
-    public void setEdited(Boolean edited) {
-        this.edited = edited;
-    }
-
-    public LocalDateTime getLastEditDate() {
-        return lastEditDate;
-    }
-
-    public void setLastEditDate(LocalDateTime lastEditDate) {
-        this.lastEditDate = lastEditDate;
-    }
-
-    public UserDto getUser() {
-        return user;
-    }
-
-    public void setUser(UserDto user) {
-        this.user = user;
-    }
-
-    public PostCategoryDto getPostCategory() {
-        return postCategory;
-    }
-
-    public void setPostCategory(PostCategoryDto postCategory) {
-        this.postCategory = postCategory;
-    }
-
-    public Collection<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(Collection<String> tags) {
-        this.tags = tags;
-    }
-
-    public String getMovies() {
+    public Collection<MovieDto> getMovies() {
         return movies;
     }
 
-    public void setMovies(String movies) {
+    public void setMovies(Collection<MovieDto> movies) {
         this.movies = movies;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public Long getTotalLikes() {
-        return totalLikes;
-    }
-
-    public void setTotalLikes(Long totalLikes) {
-        this.totalLikes = totalLikes;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-
-    public String getVotes() {
-        return votes;
-    }
-
-    public void setVotes(String votes) {
-        this.votes = votes;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
     }
 }
