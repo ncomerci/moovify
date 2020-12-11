a1b80b90b9899047f2be4e0f13d7d7fb421c1520
Nicolás <ncomerci@itba.edu.ar>
2020-12-11 12:16:13 -0300

    signup form duplicated fields check support

--- frontend/app/scripts/controllers/SignupCtrl.js
+++ frontend/app/scripts/controllers/SignupCtrl.js
@@ -1,7 +1,7 @@
 define(['frontend', 'services/LoginService', 'services/PageTitleService', 'services/RestFulResponseFactory'], function(frontend) {
 
     'use strict';
-    frontend.controller('SignupCtrl', function($scope, LoggedUserFactory, $window, PageTitle, RestFulResponse, $location, $q) {
+    frontend.controller('SignupCtrl', function($scope, LoggedUserFactory, $window, PageTitle, RestFulResponse, $location, $translate) {
       PageTitle.setTitle('asd') //TODO: cambiar la key
 
       LoggedUserFactory.isLogged().then(resp => {
@@ -33,6 +33,25 @@ define(['frontend', 'services/LoginService', 'services/PageTitleService', 'servi
         maxLen: 30
       }
 
+      const fieldErrors = {
+        name: {
+          i18nKey: 'USER_CREATE_NAME',
+          message: ''
+        },
+        email: {
+          i18nKey: 'USER_CREATE_EMAIL',
+          message: ''
+        },
+        username: {
+          i18nKey: 'USER_CREATE_USERNAME',
+          message: ''
+        },
+        password: {
+          i18nKey: 'USER_CREATE_PASSWORD',
+          message: ''
+        },
+      }
+
       // TODO: subir avatar
       $scope.signup = function (user) {
 
@@ -47,22 +66,23 @@ define(['frontend', 'services/LoginService', 'services/PageTitleService', 'servi
           !$scope.passwordsNotEquals()
         )
         {
-          RestFulResponse.all('users').post(user).then(resp => {
-            if(resp.status === 201) {
-              const aux_user = {
-                username: user.username,
-                password: user.password
+          RestFulResponse.all('users').post(user).then(() => {
+            const aux_user = {
+              username: user.username,
+              password: user.password
+            }
+            LoggedUserFactory.login(aux_user, false).then(() => {
+              $location.path(`/`) // TODO: poner la ruta del perfil
+            })
+          }).catch(err => {
+              for(const e of err.data) {
+                $translate(fieldErrors[e['attribute']].i18nKey).then(field => {
+                  $translate('FORM_DUPLICATED_FIELD_ERROR', {field: field}).then(msg => {
+                    fieldErrors[e['attribute']].message = msg;
+                  }).catch(err => console.log('inside', err));
+                }).catch(err => console.log('outside', err));
               }
-              LoggedUserFactory.login(aux_user, false).then(() => {
-                $location.path(`/`) // TODO: poner la ruta del perfil
-              })
-
-            }
-            else {
-              console.log(resp);
-            //  TODO: mostrar errores (por ej si el usuario o el mail existen)
-            }
-          })
+          });
         }
       }
 
@@ -80,7 +100,23 @@ define(['frontend', 'services/LoginService', 'services/PageTitleService', 'servi
       }
 
       $scope.passwordsNotEquals = function () {
-        return $scope.user.password !== $scope.user.repeatPassword;
+        return $scope.user.password !== $scope.user.repeatPassword && !($scope.user.password === undefined || $scope.user.repeatPassword === undefined);
+      }
+
+      $scope.checkFieldError = function (field) {
+        if(fieldErrors[field] === undefined) {
+          throw `Field '${field}' does not exists!`;
+        }
+
+        return fieldErrors[field].message !== '';
+      }
+
+      $scope.getFieldErrors = function (field) {
+        return fieldErrors[field].message;
+      }
+
+      $scope.resetFieldErrors = function (field) {
+        fieldErrors[field].message = '';
       }
 
     });
