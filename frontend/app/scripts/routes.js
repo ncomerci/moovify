ad301902cd2f78ec158f9f41306548b7b684c23a
Ignacio <isagues@itba.edu.ar>
2020-12-12 02:05:26 -0300

    Working flame

--- frontend/app/scripts/routes.js
+++ frontend/app/scripts/routes.js
@@ -5,23 +5,23 @@ define([], function() {
         defaultRoutePath: '/404',
         routes: {
             '/': {
-                templateUrl: '/views/home.html',
+                templateUrl: 'resources/views/home.html',
                 controller: 'HomeCtrl',
             },
             '/404': {
-                templateUrl: '/404.html',
+                templateUrl: 'resources/404.html',
                 // controller: 'ErrorController'
             },
             '/search': {
-                templateUrl: '/views/search/search.html',
+                templateUrl: 'resources/views/search/search.html',
                 controller: 'SearchController'
             },
             '/login': {
-                templateUrl: '/views/login/login.html',
+                templateUrl: 'resources/views/login/login.html',
                 controller: 'LoginCtrl',
             },
             '/signup': {
-              templateUrl: '/views/login/signup.html',
+              templateUrl: 'resources/views/login/signup.html',
               controller: 'SignupCtrl',
             },
             /* ===== yeoman hook ===== */
